2a0d036fec761643c687e2ca68b240f53060a592
MeghaPatil123 <52202778+MeghaPatil123@users.noreply.github.com>
2019-06-25 22:40:41 +0530

    Add files via upload

---
+++ Node_demo/add.js
@@ -0,0 +1,8 @@
+exports.evenodd=function(num)
+{
+	
+	if(parseInt(num)%2==0)
+		console.log("even number");
+	else
+		console.log("odd number");
+}
---
+++ Node_demo/function.js
@@ -0,0 +1,5 @@
+var m1=require("./add");
+
+
+var f=m1.evenodd(5);
+		console.log("odd number",+f);
